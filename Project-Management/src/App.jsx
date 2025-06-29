97cd7815e7f73e41b6d35a8e8c12ee36917820c6
Dhananjay <dhananjay.ankushrao@gmail.com>
2025-06-29 12:21:26 +0400

    Add multiple projects

---
+++ Project-Management/src/App.jsx
@@ -0,0 +1,11 @@
+import Projectsidebars from "./components/projectsidebars.jsx";
+
+function App() {
+  return (
+    <main className="h-screen my-8 my-8">
+      <Projectsidebars />
+    </main>
+  );
+}
+
+export default App;
