0c60c0caaea2ecc2030ff0b22623bb00c22b456e
Dhananjay <dhananjay.ankushrao@gmail.com>
2025-06-30 22:14:29 +0400

    Add multiple projects

--- Project-Management/src/components/newProject.jsx
+++ Project-Management/src/components/newProject.jsx
@@ -22,8 +22,6 @@ export default function NewProject({ onAdd, onCancel }) {
         }
         );
     }
-
-
     return (
         <>
             <Modal ref={modalObj}>
@@ -33,7 +31,7 @@ export default function NewProject({ onAdd, onCancel }) {
             </Modal>
             <div className='w-[35rem] mt-16'>
                 <menu className='flex items-center justify-end gap-4 my-4'>
-                    <li><button onClick={onCancel} className=' text-stone-800 hover:bg-stone-950'>Cancel</button></li>
+                    <li><button onClick={onCancel} className=' text-stone-800 hover:bg-stone-200'>Cancel</button></li>
                     <li><button onClick={handleSave} className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'>Save</button></li>
                 </menu>
                 <div>
